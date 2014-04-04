0e014347da8a79518946d6526a64f2c7141d8808
Joe Lauer <joe@lauer.bz>
2014-04-03 23:05:42 -0700

    Added property for windows

--- src/main/java/com/mfizz/jtools/launcher/Configuration.java
+++ src/main/java/com/mfizz/jtools/launcher/Configuration.java
@@ -185,6 +185,14 @@ public class Configuration {
         this.name = name;
     }
 
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+    
     public String getShortDescription() {
         return shortDescription;
     }

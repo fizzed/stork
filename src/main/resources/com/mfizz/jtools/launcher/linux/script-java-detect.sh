ccadc885f88fd7ecfcd7a7e5ea39eda4a2bbe0e4
Joe Lauer <joe@lauer.bz>
2014-04-14 12:51:39 -0700

    Updated java script to fix spaces in paths

--- src/main/resources/com/mfizz/jtools/launcher/linux/script-java-detect.sh
+++ src/main/resources/com/mfizz/jtools/launcher/linux/script-java-detect.sh
@@ -6,7 +6,7 @@
 doAllJavaExecutables()
 {
     local all_java_bins=`findAllJavaExecutables`
-    if [ -z $all_java_bins ]; then
+    if [ -z "$all_java_bins" ]; then
         echo "No java executables found"
         exit 2
     else
@@ -18,15 +18,20 @@ doFirstMinVersion()
 {
     local all_java_bins=`findAllJavaExecutables`
     local min_version="$1"
-    if [ -z $min_version ]; then
+    if [ -z "$min_version" ]; then
         echo "parameter 2 required to be minimum java version (e.g. 1.7)"
         exit 1
     fi
 
     local java_bin=`findFirstJavaExecutableByMinimumMajorVersion "$all_java_bins" "$min_version"`
-    if [ -z $java_bin ]; then
-        echo "No java executables found"
-        exit 2
+    if [ -z "$java_bin" ]; then
+        if [ ! -z $all_java_bins ]; then
+            echo "Java executables found but all < $min_version"
+            exit 3
+        else
+            echo "No java executables found"
+            exit 2
+        fi
     else
         echo "$java_bin"
     fi
@@ -36,7 +41,7 @@ doLatestMajorVersion()
 {
     local all_java_bins=`findAllJavaExecutables`
     local java_bin=`findLatestJavaExecutableByMajorVersion "$all_java_bins"`
-    if [ -z $java_bin ]; then
+    if [ -z "$java_bin" ]; then
         echo "No java executables found"
         exit 2
     else
@@ -77,7 +82,10 @@ case "$1" in
     ;;
 
   *)
-    echo "Usage: $0 " >&2
+    echo "Usage: `basename $0` [command] [optional args per command]" >&2
+    #echo " --all-java-executables               Prints colon delimited list of" >&2
+    #echo "                                      all javas on system" >&2
+    #echo " --first-min-version [min-version]    " >&2
     exit 1
     ;;
 
